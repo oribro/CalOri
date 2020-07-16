19651c258fec07b02d272df8483e95d47f891c42
Ori Broda <ori.broda@mail.huji.ac.il>
2020-07-16 23:13:25 +0300

    Fixes

--- source.js
+++ source.js
@@ -1,9 +1,11 @@
-function buildTable(start, days) {
-    let table = document.getElementsByTagName("table")[0];
+let table = document.getElementsByTagName("table")[0];
+const DAYS_A_WEEK = 7;
+
+function buildTable(table, start, days, weeks) {
     let curDay = 1;
-    for (let i = 1; i <= 6; i++) {
+    for (let i = 1; i <= weeks; i++) {
         let tr = table.insertRow();
-        for (let j = 1; j <= 7; j++) {
+        for (let j = 1; j <= DAYS_A_WEEK; j++) {
             let td = tr.insertCell();
             td.appendChild(document.createTextNode('Cell'));
             if ((i === 1 && j < start) || curDay > days)
@@ -19,26 +21,35 @@ function buildTable(start, days) {
 }
 
 function createMonth(month) {
-    let start, days;
+    let start, days, weeks;
     switch(month) {
         case 7:
             start = 4;
             days = 31;
+            weeks = 5;
             break;
         case 8:
             start = 7;
             days = 31;
+            weeks = 6;
             break;
         case 9:
             start = 3;
             days = 30;
+            weeks = 5;
             break;
         case 10:
             start = 5;
             days = 31;
+            weeks = 5;
             break;
     }
-    buildTable(start, days);
+    return {
+        start: start,
+        days: days,
+        weeks: weeks
+    };
 }
 
-createMonth(7);
+const {start, days, weeks} = createMonth(7);
+buildTable(table, start, days, weeks);
